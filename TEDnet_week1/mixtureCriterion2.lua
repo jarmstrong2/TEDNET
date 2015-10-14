524288575d53ddd201a479f8c99ed796ad85b5e3
John Armstrong <j2armstrong@gmail.com>
2015-10-13 23:17:46 -0400

    Update mixtureCriterion2.lua

--- TEDnet_week1/mixtureCriterion2.lua
+++ TEDnet_week1/mixtureCriterion2.lua
@@ -171,7 +171,7 @@ function MixtureCriterion:updateGradInput(input, target)
         -- COMPUTE dL(x)/d(pi_t_hat)
         --local d_pi_t_hat = torch.cmul(gamma, (torch.add(pi_t, -1)))
         --local d_pi_t_hat = pi_t - gamma
-        local d_pi_t_hat = torch.mul(torch.cmul(gamma, (torch.pow(pi_t, -1))), -1)
+        local d_pi_t_hat = torch.mul(torch.cmul(gamma, (torch.pow(torch.add(pi_t, 1e-10)), -1))), -1)
     
         -- COMPUTE dL(x)/d(mu_t_hat)
         local dl_mu_t_hat = torch.cmul(xMinusMu, sigmaTensorInverse)
