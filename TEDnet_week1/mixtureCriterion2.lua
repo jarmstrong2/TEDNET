8c809f4f16f517806803d53e2addf0faaf1b1162
John Armstrong <j2armstrong@gmail.com>
2015-10-13 20:04:39 -0400

    Update mixtureCriterion2.lua

--- TEDnet_week1/mixtureCriterion2.lua
+++ TEDnet_week1/mixtureCriterion2.lua
@@ -142,7 +142,7 @@ function MixtureCriterion:updateGradInput(input, target)
         -- multiplied by respective mixture components
         local gammaHat = self:getMixMultVarGauss(sigma_t, mu_t, pi_t, xTarget, batchsize)
         
-        local sumGammaHat = mixGauss:sum(2)
+        local sumGammaHat = gammaHat:sum(2)
     
         -- expand to size of matrix gammaHat in order to compute gamma components
         -- for each entry
