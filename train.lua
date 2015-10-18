0e2145d6e28a328eb527c264109bf8f577568969
jarmstong2 <j2armstrong@gmail.com>
2015-10-18 16:51:01 -0400

    train.lua

--- train.lua
+++ train.lua
@@ -261,12 +261,12 @@ for i = 1, iterations do
 
     local _, loss = optim.adam(feval, params, optim_state)
 
-    print(string.format("update param, loss = %6.8f, gradnorm = %6.4e", loss[1], grad_params:clone():norm()))
+    --print(string.format("update param, loss = %6.8f, gradnorm = %6.4e", loss[1], grad_params:clone():norm()))
     if i % 20 == 0 then
-        print(string.format("iteration %4d, loss = %6.8f, gradnorm = %6.4e", i, loss[1], grad_params:norm()))
+        --print(string.format("iteration %4d, loss = %6.8f, gradnorm = %6.4e", i, loss[1], grad_params:norm()))
         valLoss = getValLoss()
         vallosses[#vallosses + 1] = valLoss
-        print(string.format("validation loss = %6.8f", valLoss))
+        --print(string.format("validation loss = %6.8f", valLoss))
         if minValLoss > valLoss then
             minValLoss = valLoss
             torch.save("tednet.t7", model)
